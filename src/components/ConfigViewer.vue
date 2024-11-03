a2e7d697ef3df29b49b64e73a2c1c3d624e98735
G992 <pvapvpa2014@yandex.ru>
2024-11-03 10:24:04 +0300

    Update version to 0.0.4 and add download functionality for bot configuration.

--- src/components/ConfigViewer.vue
+++ src/components/ConfigViewer.vue
@@ -9,16 +9,32 @@ const $q = useQuasar();
 
 // calling here; equivalent to when component is created
 $q.dark.set(true);
+
+const downloadConfig = async () => {
+  const dataStr =
+    'data:text/json;charset=utf-8,' +
+    encodeURIComponent(JSON.stringify(props.config));
+  const dlAnchorElem = document.getElementById('downloadAnchorElem');
+  if (!dlAnchorElem) return;
+  dlAnchorElem.setAttribute('href', dataStr);
+  dlAnchorElem.setAttribute(
+    'download',
+    `BOT_CONFIG_${new Date().toLocaleString()}.json`
+  );
+  dlAnchorElem.click();
+};
 </script>
 
 <template>
   <div class="column q-mt-md">
+    <a id="downloadAnchorElem" style="display: none" />
+    <q-btn label="Скачать" @click="downloadConfig" class="q-mb-sm" flat />
     <json-viewer
       :value="{ config: props.config, pnl_update_delay: 1000 }"
       :expand-depth="5"
       copyable
       theme="dark"
-    ></json-viewer>
+    />
   </div>
 </template>
 
